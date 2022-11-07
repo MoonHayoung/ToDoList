63241b3de5a070720ac3ebaa6964d873487f3bf4
jeonjihoo0131 <82494717+jeonjihoo0131@users.noreply.github.com>
2022-11-07 12:50:52 +0900

    Update todoList.js

--- todoList.js
+++ todoList.js
@@ -1,23 +1,21 @@
-setTimeout(function (interval) {
+setTimeout(function(interval) {
     clearInterval(interval);
-}, setInterval(function () {
-        
-    let today = new Date();   
+}, setInterval(function() {
 
-    let year = today.getFullYear(); 
-    let month = ("0" + (today.getMonth() + 1)).slice(-2);  
-    let date = ("0" + today.getDate()).slice(-2);  
-    let day = today.getDay();  
+    let today = new Date();
 
-    let hour = today.getHours(); 
-    let minute = ("0" + today.getMinutes()).slice(-2); 
-    // 초 let time = ("0" + today.getSeconds()).slice(-2);
+    let year = today.getFullYear();
+    let month = ("0" + (today.getMonth() + 1)).slice(-2);
+    let date = ("0" + today.getDate()).slice(-2);
+    let day = today.getDay();
 
-    today_write =  ('<p>'+ year + '-' + month + '-' + date +'</p>');
-    time_write = ('<p>'+ hour + ':'+ minute + '</p>');
+    let hour = today.getHours();
+    let minute = ("0" + today.getMinutes()).slice(-2);
+    let time = ("0" + today.getSeconds()).slice(-2);
+
+    today_write = (`<p> ${year}-${month}-${date}</p>`);
+    time_write = (`<p> ${hour}:${minute}:${time} </p>`);
 
     document.getElementById("date").innerHTML = today_write;
     document.getElementById("time").innerHTML = time_write;
-}, 1)); 
-
-
+}, 1));
